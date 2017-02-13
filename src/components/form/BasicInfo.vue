66c1e6cf73e5d001a408f9ef5adcfa513d21b5e2
Kunal Varma <kunalvarma05@gmail.com>
2017-02-14 01:37:24 +0530

    BasicInfo Form

--- src/components/form/BasicInfo.vue
+++ src/components/form/BasicInfo.vue
@@ -1,9 +1,32 @@
 <template lang="html">
     <div class="form basic-info-form">
-        <label class="label">Full Name</label>
-        <p class="control">
-          <input class="input" type="text" placeholder="Full Name" v-model="name">
-        </p>
+        <div class="form-input">
+            <label class="label">Name</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="Full Name" v-model="name">
+            </p>
+        </div>
+
+        <div class="form-input">
+            <label class="label">Email</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="DAIICT Email ID" v-model="email">
+            </p>
+        </div>
+
+        <div class="form-input">
+            <label class="label">Date of Birth</label>
+            <p class="control">
+              <input class="input" type="text" placeholder="April 01, 1991" v-model="dob">
+            </p>
+        </div>
+
+        <div class="form-input">
+            <label class="label">Address</label>
+            <p class="control">
+              <textarea class="textarea input" type="text" placeholder="Address" v-model="address"></textarea>
+            </p>
+        </div>
     </div>
 </template>
 
