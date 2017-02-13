5e703e1317d806642b1ebdb193760cb08c5b584c
Kunal Varma <kunalvarma05@gmail.com>
2017-02-14 03:40:39 +0530

    Event Bus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 
+// Create a global Event Bus
+var EventBus = new Vue();
+
+// Add to Vue properties by exposing a getter for $bus
+Object.defineProperties(Vue.prototype, {
+  $bus: {
+    get: function() {
+      return EventBus;
+    }
+  }
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
