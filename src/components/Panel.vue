c767c69855dff649490a4e5a17c179d5a06fb09c
Kunal Varma <kunalvarma05@gmail.com>
2017-02-15 15:55:18 +0530

    Panel toggle group

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-<div :class="{ panel: true, open: !isHidden}">
+<div :class="{ panel: true, open: !isHidden}" :id="id">
     <div class="panel-header" @click="toggle">
         <p>{{ title }} <span :class="{ 'fa icon': true, 'fa-chevron-down': isHidden, 'fa-chevron-up': !isHidden }"></span></p>
     </div>
@@ -12,6 +12,16 @@
 <script>
 export default {
     name: 'panel',
+
+    created () {
+        this.$bus.$on('panel-open', (data) => {
+            if (this.group === data.group) {
+                if (this.id !== data.id) {
+                    this.isHidden = true;
+                }
+            }
+        });
+    },
 
     data () {
         return {
@@ -27,14 +37,25 @@ export default {
         isOpen: {
             type: Boolean,
             default: false
+        },
+        id: {
+            type: String,
+            required: true
+        },
+        group: {
+            type: String,
+            required: true
         }
+
+
     },
 
     methods: {
         toggle (event) {
             this.isHidden = !this.isHidden;
+            this.$bus.$emit('panel-open', { group: this.group, id: this.id});
         }
-    }
+    },
 
 }
 </script>
--- src/components/SidebarForm.vue
+++ src/components/SidebarForm.vue
@@ -1,26 +1,26 @@
 <template lang="html">
     <form class="sidebar-form">
-        <panel title="Personal Info" :isOpen="false" >
+        <panel group="sidebar-panel" id="personal-info" title="Personal Info" :isOpen="false" >
             <personal-info :info="student.info"></personal-info>
         </panel>
 
-        <panel title="Education" >
+        <panel group="sidebar-panel" id="education-info" title="Education" >
             <education-info :degrees="student.degrees"></education-info>
         </panel>
 
-        <panel title="Skills" >
+        <panel group="sidebar-panel" id="skill-info" title="Skills" >
             <skill-info :skill="student.skill"></skill-info>
         </panel>
 
-        <panel title="Projects" >
+        <panel group="sidebar-panel" id="project-info" title="Projects" >
             <project-info :projects="student.projects"></project-info>
         </panel>
 
-        <panel title="Awards and Achievements" >
+        <panel group="sidebar-panel" id="award-info" title="Awards and Achievements" >
             <award-info :awards="student.awards"></award-info>
         </panel>
 
-        <panel title="Interests and Hobbies" >
+        <panel group="sidebar-panel" id="hobby-info" title="Interests and Hobbies" >
             <hobby-info :hobbies="student.hobbies"></hobby-info>
         </panel>
     </form>
