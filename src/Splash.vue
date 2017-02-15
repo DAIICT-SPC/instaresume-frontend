97dbbca8ec412c0180dc97da0a822edac2f65fcc
Kunal Varma <kunalvarma05@gmail.com>
2017-02-15 23:55:57 +0530

    Splash page

---
+++ src/Splash.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div id="splash" class="splash">
+      <div class="intro">
+          <a href="index.html" class="logo">
+              <img src="./assets/logo.svg" :alt="title" />
+              <span>{{ title }}</span>
+          </a>
+          <!-- <p class="tagline">
+              {{tagline}}
+          </p> -->
+          <p>
+              <a class="button is-lighter is-large signin-button" type="button" @click="signIn"> <span class="fa icon fa-google"></span> Sign in with Google</a>
+          </p>
+      </div>
+      <img src="./assets/resume.svg" alt="InstaResume" class="illustration">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'splash',
+
+  data() {
+    return {
+      title: "InstaResume",
+      tagline: "Instant resume generator."
+    };
+  },
+
+  methods: {
+    signIn() {
+      const user = {};
+      this.$bus.$emit("user-authenticated", user);
+    }
+  }
+
+}
+</script>
