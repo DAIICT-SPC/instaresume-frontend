e8bd73ff26cd4a66f97057361fd0962487d11c85
Kunal Varma <kunalvarma05@gmail.com>
2017-02-21 15:19:19 +0530

    Environment config

--- src/config.js
+++ src/config.js
@@ -1,20 +1,11 @@
 let config = require('../config');
+import Env from './env'
 
 export default {
-  production: {
-      apiUrl: "http://backend.instaresume.kunalvarma.in/api",
-      token: '$2y$10$cEOiWUjwt9Pn0I7m2HdeOcYWOFZM9uNKYEjSRTYSycOMXhxTLz6'
-  },
-
-  development: {
-      apiUrl: "http://identity.app/api",
-      token: 'tHdUjw9PncYWOFZM9uNKYEOEOiWeOcMXhxTLz6jSRTYSyc'
-  },
+  apiUrl: Env.apiUrl,
+  token: Env.apiKey,
 
   get(key) {
-    if (process.env.NODE_ENV === "production")
-      return this.production[key];
-    else
-      return this.development[key];
+    return this[key];
   }
 }
