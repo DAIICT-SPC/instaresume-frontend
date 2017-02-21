ee9a7b252b445fb8c4e7f16974329ced302e671d
Kunal Varma <kunalvarma05@gmail.com>
2017-02-21 12:52:21 +0530

    Use environment config file

--- src/config.js
+++ src/config.js
@@ -1,18 +1,10 @@
 let config = require('../config');
+import Env from './env'
+
+let api_key = Env.apiKey;
+let api_url = Env.apiUrl;
 
 export default {
-  production: {
-      apiUrl: "http://backend.instaresume.kunalvarma.in/api"
-  },
-
-  development: {
-      apiUrl: "http://identity.app/api"
-  },
-
-  get(key) {
-    if (process.env.NODE_ENV === "production")
-      return this.production[key];
-    else
-      return this.development[key];
-  }
+  apiUrl: api_url,
+  apiKey: api_key
 }
