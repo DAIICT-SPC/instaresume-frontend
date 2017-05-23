c94588244dde92c1b4e3a5add1d5942cbbda332c
Kunal Varma <kunalvarma05@gmail.com>
2017-05-24 00:12:48 +0530

    Placeholder for Position

--- src/components/form/Position.vue
+++ src/components/form/Position.vue
@@ -1,7 +1,7 @@
 <template lang="html">
     <div class="form position-form">
         <div class="form-input">
-            <input type="text" :placeholder="'Position of Responsibility #' + (id + 1)" class="input" v-model="position.name" @keydown.8="removePosition(id)" />
+            <input type="text" placeholder="Member of XYZ Committee from 1st January, 2017 to 10th May 2017" class="input" v-model="position.name" @keydown.8="removePosition(id)" />
         </div>
     </div>
 </template>
